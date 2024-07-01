ce3cbada8100019190b904a91375383f02d977f3
N0153128 <noisebro@hotmail.com>
2024-07-01 21:24:00 +0100

    MOTD now automatically creates Log directory upon start

--- scenarios/motd/tools/logmod.py
+++ scenarios/motd/tools/logmod.py
@@ -6,6 +6,7 @@ class Loger:
 
     @staticmethod
     async def log_saver(name, uid, message, ctype, cid, uname):
+        os.mkdir('./logs')
         logging.basicConfig(filename=f'{os.path.dirname(os.path.abspath(__file__))}/../logs/general.log', level=logging.INFO,
                             format='%(asctime)s:%(message)s')
         logging.info('TYPE {}-{} BY @{}-{}, ID {} : {}'.format(ctype, cid, uname, name, uid, message))
